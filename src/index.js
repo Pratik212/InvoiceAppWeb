15738c22bfa192e08f475fd50a1a4d42fb421cdf
pratik palanpure <pratikpalanpure212@gmail.com>
2021-06-22 00:11:53 +0530

    Set up project.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import AppContext from './AppContext';
+import Routes from './app/routes/index';
+import {Router} from "react-router-dom";
+import history from './@history';
+import routes from "./app/fuse-configs/routesConfig";
+import { Provider } from 'react-redux';
+import store from "./store";
+import Auth from "./app/auth/Auth";
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <AppContext.Provider
+        value={{
+            routes
+        }}
+    >
+        <Provider store={store}>
+            <Auth>
+                <Router history={history}>
+                    <Routes />
+                </Router>
+            </Auth>
+        </Provider>
+    </AppContext.Provider>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorker.unregister();
